187c3856aaa92c69b7d8587d3ec89e1fbc4288cf
ndittren <36773036+ndittren@users.noreply.github.com>
2021-05-13 12:46:36 -0400

    Initial migration

--- quizcon/urls.py
+++ quizcon/urls.py
@@ -1,30 +1,29 @@
 from django.urls import include, path
+from django.conf.urls import url
 from django.contrib import admin
 from django.conf import settings
 from django.views.generic import TemplateView
 from django.views.static import serve
-from django_cas_ng import views as cas_views
 from quizcon.main import views
 
 admin.autodiscover()
 
+auth_urls = url(r'^accounts/', include('django.contrib.auth.urls'))
+if hasattr(settings, 'CAS_BASE'):
+    auth_urls = url(r'^accounts/', include('djangowind.urls'))
 
 urlpatterns = [
+    auth_urls,
     path('', views.IndexView.as_view()),
     path('admin/', admin.site.urls),
 
     path('accounts/', include('django.contrib.auth.urls')),
-    path('cas/login', cas_views.LoginView.as_view(),
-         name='cas_ng_login'),
-    path('cas/logout', cas_views.LogoutView.as_view(),
-         name='cas_ng_logout'),
 
     path('_impersonate/', include('impersonate.urls')),
     path('stats/', TemplateView.as_view(template_name="stats.html")),
     path('smoketest/', include('smoketest.urls')),
     path('infranil/', include('infranil.urls')),
-    path('uploads/<str:path>',
-        serve, {'document_root': settings.MEDIA_ROOT}),
+    path('uploads/<str:path>', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
 

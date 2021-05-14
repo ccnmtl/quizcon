1309127685b5a48072679589be6ba2eb219e32ff
Susan Dreher <sdreher@gmail.com>
2021-05-14 15:05:00 -0400

    Default Django Rest Framework Settings

--- quizcon/settings_shared.py
+++ quizcon/settings_shared.py
@@ -51,3 +51,15 @@ LTI_TOOL_CONFIGURATION = {
 }
 
 COURSEAFFILS_COURSESTRING_MAPPER = CourseStringMapper
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
+}

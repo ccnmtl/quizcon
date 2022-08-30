215721b48e9d6ce8125b75c43acd0e3a8af4ff42
ndittren <36773036+ndittren@users.noreply.github.com>
2022-08-30 15:22:32 -0400

    add help icon tooltip

--- media/js/src/utils.js
+++ media/js/src/utils.js
@@ -4,4 +4,9 @@ function copyToClipboard(eltId) {
     var text = document.getElementById(eltId);
     text.select(); //select the text area
     document.execCommand('copy'); //copy to clipboard
-}
+};
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})

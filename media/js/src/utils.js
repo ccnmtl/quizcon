899cff129c751e5ab22bfe77cbc1e45bfaa4433c
ndittren <36773036+ndittren@users.noreply.github.com>
2022-08-30 16:03:23 -0400

    add help icon to quiz create forms

--- media/js/src/utils.js
+++ media/js/src/utils.js
@@ -10,3 +10,25 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
+jQuery("#id_show_answers_2").on("click", function() {
+    jQuery("#id_show_answers_date").attr("disabled", false)
+});
+jQuery("#id_show_answers_1").on("click", function() {
+    jQuery("#id_show_answers_date").attr("disabled", true)
+});
+jQuery("#id_show_answers_0").on("click", function() {
+    jQuery("#id_show_answers_date").attr("disabled", true)
+});
+jQuery(document).ready(function() {
+    if(jQuery('#id_show_answers_2').is(':checked')) {
+        jQuery("#id_show_answers_date").attr("disabled", false)
+    }
+});
+jQuery("#timerSwitch").on("click", function() {
+    if(jQuery("#timerSwitch").is(":checked")){
+        jQuery("#id_time").attr("disabled", false)
+    } else {
+        jQuery("#id_time").attr("disabled", true)
+    }
+});

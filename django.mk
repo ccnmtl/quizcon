94bd50a744441de9bb1022f1cebea0f271a5d5a0
Nikolas Nyby <nnyby@columbia.edu>
2025-12-10 14:32:19 -0500

    Update makefiles

--- django.mk
+++ django.mk
@@ -2,22 +2,22 @@
 
 # CHANGES:
 # 1.10.0 - 2025-10-16 - Removed coverage, updated dependencies.
-# 1.9.0              - Use coverage tool directly to generate coverage
-#                      reports.
-#                    - wheel and pip updates
-#                    - Use pre-compiled binary wheel for cryptography
-#					 - Remove Travis references
-# 1.8.0 - 2019-10-21 - Don't run flake8 on local_settings.py
-# 1.7.0 - 2018-05-31 - Now using python 3 by default
-#                    - Removed virtualenv.py in favor of python 3's
-#                      builtin venv capability.
-# 1.6.0 - 2017-09-05 - add bandit secure analysis configuration
-# 1.5.0 - 2017-08-24 - remove jshint/jscs in favor of eslint
-# 1.4.0 - 2017-06-06 - backout the switch to eslint. that's not really ready yet.
-# 1.3.0 - 2017-06-05 - pypi location is not needed anymore
-# 1.2.0 - 2016-12-15 - bump wheel version to 0.29
-# 1.1.0 - 2016-11-08 - run flake8 tests before unit tests
-# 1.0.1 - 2016-05-02 - Remove deprecated syncdb command from make install
+# 1.9.0               - Use coverage tool directly to generate coverage
+#                       reports.
+#                     - wheel and pip updates
+#                     - Use pre-compiled binary wheel for cryptography
+#                     - Remove Travis references
+# 1.8.0  - 2019-10-21 - Don't run flake8 on local_settings.py
+# 1.7.0  - 2018-05-31 - Now using python 3 by default
+#                     - Removed virtualenv.py in favor of python 3's
+#                       builtin venv capability.
+# 1.6.0  - 2017-09-05 - add bandit secure analysis configuration
+# 1.5.0  - 2017-08-24 - remove jshint/jscs in favor of eslint
+# 1.4.0  - 2017-06-06 - backout the switch to eslint. that's not really ready yet.
+# 1.3.0  - 2017-06-05 - pypi location is not needed anymore
+# 1.2.0  - 2016-12-15 - bump wheel version to 0.29
+# 1.1.0  - 2016-11-08 - run flake8 tests before unit tests
+# 1.0.1  - 2016-05-02 - Remove deprecated syncdb command from make install
 
 VE ?= ./ve
 MANAGE ?= ./manage.py
@@ -25,7 +25,8 @@ REQUIREMENTS ?= requirements.txt
 SYS_PYTHON ?= python3
 PY_SENTINAL ?= $(VE)/sentinal
 WHEEL_VERSION ?= 0.45.1
-PIP_VERSION ?= 25.2
+PIP_VERSION ?= 25.3
+SETUPTOOLS_VERSION ?= 80.9.0
 MAX_COMPLEXITY ?= 10
 INTERFACE ?= localhost
 RUNSERVER_PORT ?= 8000
@@ -34,19 +35,19 @@ BANDIT ?= $(VE)/bin/bandit
 FLAKE8 ?= $(VE)/bin/flake8
 PIP ?= $(VE)/bin/pip
 
-jenkins: check flake8 test eslint bandit
+jenkins: check flake8 test bandit
 
 $(PY_SENTINAL): $(REQUIREMENTS)
 	rm -rf $(VE)
 	$(SYS_PYTHON) -m venv $(VE)
 	$(PIP) install pip==$(PIP_VERSION)
-	$(PIP) install --upgrade setuptools
+	$(PIP) install setuptools==$(SETUPTOOLS_VERSION)
 	$(PIP) install wheel==$(WHEEL_VERSION)
 	$(PIP) install --no-deps --requirement $(REQUIREMENTS)
 	touch $@
 
 test: $(PY_SENTINAL)
-	$(MANAGE) test $(APP)
+	$(MANAGE) test $(PY_DIRS)
 
 parallel-tests: $(PY_SENTINAL)
 	$(MANAGE) test --parallel
@@ -77,26 +78,4 @@ clean:
 	rm -rf node_modules
 	find . -name '*.pyc' -exec rm {} \;
 
-pull:
-	git pull
-	make check
-	make test
-	make migrate
-	make flake8
-
-rebase:
-	git pull --rebase
-	make check
-	make test
-	make migrate
-	make flake8
-
-# run this one the very first time you check
-# this out on a new machine to set up dev
-# database, etc. You probably *DON'T* want
-# to run it after that, though.
-install: jenkins
-	createdb $(APP)
-	make migrate
-
-.PHONY: jenkins test flake8 runserver migrate check shell clean pull rebase install
+.PHONY: jenkins test flake8 runserver migrate check shell clean
